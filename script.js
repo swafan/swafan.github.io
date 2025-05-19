3b25dbbc710bb6cfecad74417bce004722bd9b2a
swafan <alvin.shuwai@gmail.com>
2025-05-19 14:34:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// Fade in-out
+
+const title = document.querySelector("#intro");
+const bioEnd = document.querySelector("#intro-end-marker");
+
+const observer = new IntersectionObserver(
+  ([entry]) => {
+    if (entry.isIntersecting) {
+      // User is scrolling UP (title should reappear)
+      title.classList.remove("fade-out");
+      title.classList.add("fade-in");
+    } else {
+      // User scrolled PAST bio-end (title fades out)
+      title.classList.remove("fade-in");
+      title.classList.add("fade-out");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80% 0px 0px 0px",
+  }
+);
+
+observer.observe(bioEnd);
+
+// Filters
+
   const filterButtons = document.querySelectorAll('.filter-buttons button');
   const writingBoxes = document.querySelectorAll('.writing-box');
 
@@ -19,4 +47,3 @@
       });
     });
   });
-
